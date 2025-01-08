00f1b0f1ea03f435efa5fbaf5234d79d957e592e
Oleksandra Kuziura <radioday2014@gmail.com>
2025-01-08 13:42:21 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const btnRowPlus = document.querySelector('.append-row');
+const btnRowMinus = document.querySelector('.remove-row');
+const btnColPlus = document.querySelector('.append-column');
+const btnColMinus = document.querySelector('.remove-column');
+const table = document.querySelector('.field');
+const trMain = document.getElementsByTagName('tr');
+
+btnRowPlus.onclick = () => {
+  const tr = document.createElement('tr');
+
+  for (let i = 1; i <= trMain[0].cells.length; i++) {
+    const td = document.createElement('td');
+
+    tr.appendChild(td);
+  }
+  table.appendChild(tr);
+};
+
+btnRowMinus.onclick = () => {
+  const el = document.getElementsByTagName('tr');
+
+  el[0].remove();
+};
+
+btnColPlus.onclick = () => {
+  for (let i = 0; i < table.rows.length; i++) {
+    const td = document.createElement('td');
+
+    trMain[i].appendChild(td);
+  }
+};
+
+btnColMinus.onclick = () => {
+  for (let i = 0; i < table.rows.length; i++) {
+    const td = trMain[i].cells;
+
+    td[table.rows.length - 1].remove();
+  }
+};
+//
