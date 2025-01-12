b44403883da9fe81dccab9653e286a32d8bfac28
Oleksandra Kuziura <radioday2014@gmail.com>
2025-01-12 16:23:31 +0200

    add disable button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,9 +10,11 @@ const trMain = document.getElementsByTagName('tr');
 btnRowPlus.onclick = () => {
   const tr = document.createElement('tr');
 
-  // console.log(trMain);
+  if (trMain.length > 2 || btnRowMinus.hasAttribute('disabled')) {
+    btnRowMinus.removeAttribute('disabled');
+  }
 
-  if (trMain.length > 0) {
+  if (trMain.length > 0 && trMain.length < 10) {
     for (let i = 1; i <= trMain[0].cells.length; i++) {
       const td = document.createElement('td');
 
@@ -20,31 +22,61 @@ btnRowPlus.onclick = () => {
     }
     table.appendChild(tr);
   }
+
+  if (trMain.length >= 10) {
+    btnRowPlus.setAttribute('disabled', '');
+  }
 };
 
 btnRowMinus.onclick = () => {
-  if (trMain.length > 4) {
+  if (trMain.length <= 10 || btnRowPlus.hasAttribute('disabled')) {
+    btnRowPlus.removeAttribute('disabled');
+  }
+
+  if (trMain.length > 2) {
     const el = document.getElementsByTagName('tr');
 
     el[el.length - 1].remove();
   }
+
+  if (trMain.length <= 2) {
+    btnRowMinus.setAttribute('disabled', '');
+  }
 };
 
 btnColPlus.onclick = () => {
-  for (let i = 0; i < table.rows.length; i++) {
-    const td = document.createElement('td');
+  if (trMain[0].cells.length > 2 || btnColMinus.hasAttribute('disabled')) {
+    btnColMinus.removeAttribute('disabled');
+  }
 
-    trMain[i].appendChild(td);
+  if (trMain[0].cells.length < 10) {
+    for (let i = 0; i < table.rows.length; i++) {
+      const td = document.createElement('td');
+
+      trMain[i].appendChild(td);
+    }
+  }
+
+  if (trMain[0].cells.length >= 10) {
+    btnColPlus.setAttribute('disabled', '');
   }
 };
 
 btnColMinus.onclick = () => {
-  if (trMain[0].cells.length > 4) {
+  if (trMain[0].cells.length <= 10 || btnColPlus.hasAttribute('disabled')) {
+    btnColPlus.removeAttribute('disabled');
+  }
+
+  if (trMain[0].cells.length > 2) {
     for (let i = 0; i < table.rows.length; i++) {
       const td = trMain[i].cells;
 
       td[td.length - 1].remove();
     }
+  }
+
+  if (trMain[0].cells.length <= 2) {
+    btnColMinus.setAttribute('disabled', '');
   }
 };
 //
